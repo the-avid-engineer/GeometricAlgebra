368fa73a8313be3d40fbc35620b2f5b160b29349
Chris Philips <the.avid.engineer@outlook.com>
2023-07-30 18:31:24 -0700

    test: tests for biquaternions

--- src/GeometricAlgebra.Common/Algebras/BiquaternionAlgebra.cs
+++ src/GeometricAlgebra.Common/Algebras/BiquaternionAlgebra.cs
@@ -3,4 +3,26 @@
 namespace GeometricAlgebra.Common.Algebras;
 
 [GeometricAlgebra(N = 2, ComponentType = typeof(ComplexAlgebra))]
-public partial record struct BiquaternionAlgebra;
+public partial record struct BiquaternionAlgebra
+{
+    public static BiquaternionAlgebra Product(BiquaternionAlgebra biquaternionValue, ComplexAlgebra complexValue)
+    {
+        return new BiquaternionAlgebra
+        {
+            S = biquaternionValue.S * complexValue,
+            N1 = biquaternionValue.N1 * complexValue,
+            N2 = biquaternionValue.N2 * complexValue,
+            N1N2 = biquaternionValue.N1N2 * complexValue,
+        };
+    }
+
+    public static BiquaternionAlgebra operator *(BiquaternionAlgebra biquaternionValue, ComplexAlgebra complexValue)
+    {
+        return Product(biquaternionValue, complexValue);
+    }
+
+    public static BiquaternionAlgebra operator *(ComplexAlgebra complexValue, BiquaternionAlgebra biquaternionValue)
+    {
+        return Product(biquaternionValue, complexValue);
+    }
+}
---
+++ test/unit/GeometricAlgebra.Tests/Algebras/BiquaternionAlgebraTests.cs
@@ -0,0 +1,53 @@
+﻿using GeometricAlgebra.Common.Algebras;
+
+namespace GeometricAlgebra.Common.Tests.Algebras;
+
+public class BiquaternionAlgebraTests
+{
+    public static IEnumerable<(BiquaternionAlgebra left, BiquaternionAlgebra right)> EqualityCases()
+    {
+        var C1 = new ComplexAlgebra(S: 1);
+        var Ci = new ComplexAlgebra(N1: 1);
+
+        var H1 = new BiquaternionAlgebra(S: C1);
+        var Hi = new BiquaternionAlgebra(N1: C1);
+        var Hj = new BiquaternionAlgebra(N2: C1);
+        var Hk = new BiquaternionAlgebra(N1N2: C1);
+
+        yield return (Hi ^ 2, -H1);
+        yield return (Hj ^ 2, -H1);
+        yield return (Hk ^ 2, -H1);
+
+        yield return ((Hi * Ci) ^ 2, H1);
+        yield return ((Hj * Ci) ^ 2, H1);
+        yield return ((Hk * Ci) ^ 2, H1);
+
+        yield return (Hi * Hj, Hk);
+        yield return (Hj * Hk, Hi);
+        yield return (Hk * Hi, Hj);
+
+        yield return (Hi * Hj * Ci, Hk * Ci);
+        yield return (Hj * Hk * Ci, Hi * Ci);
+        yield return (Hk * Hi * Ci, Hj * Ci);
+
+        yield return (Hj * Hi, -Hk);
+        yield return (Hk * Hj, -Hi);
+        yield return (Hi * Hk, -Hj);
+
+        yield return (Hj * Hi * Ci, -Hk * Ci);
+        yield return (Hk * Hj * Ci, -Hi * Ci);
+        yield return (Hi * Hk * Ci, -Hj * Ci);
+    }
+
+    public static IEnumerable<object[]> BoxedEqualityCases()
+    {
+        return EqualityCases().Select(pair => new object[] { pair.left, pair.right });
+    }
+
+    [Theory]
+    [MemberData(nameof(BoxedEqualityCases))]
+    public void EqualityTests(BiquaternionAlgebra left, BiquaternionAlgebra right)
+    {
+        Assert.Equal(left, right);
+    }
+}
